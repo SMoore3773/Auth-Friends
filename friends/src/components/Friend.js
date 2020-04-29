f5ad1e746de95733a6b203187f9bc02eb5567dd2
SMoore3773 <Smooresculpture@gmail.com>
2020-04-28 23:11:59 -0400

    tweaked friend card

--- friends/src/components/Friend.js
+++ friends/src/components/Friend.js
@@ -4,9 +4,9 @@ const Friend = props =>{
     console.log('props in friend component',props)
     return(
         <div className='friendCard'>
-            <div className='friendName'>{props.name}</div>
-            <div className='friendAge'>{props.age}</div>
-            <div className='fiendEmail'>{props.email}</div>
+            <div className='friendAttr'>{props.name}</div>
+            <div className='friendAttr'>{props.age}</div>
+            <div className='fiendAttr'>{props.email}</div>
         </div>
     )
 }
